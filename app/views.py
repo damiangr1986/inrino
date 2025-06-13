dc85cf98ac9f876425bbe2968e292b8f1fa4c40e
Alan Inglesi <alaninglesi@gmail.com>
2025-06-13 16:29:18 -0300

    lista de favorito

--- app/views.py
+++ app/views.py
@@ -11,7 +11,7 @@ def index_page(request):
 # esta función obtiene 2 listados: uno de las imágenes de la API y otro de favoritos, ambos en formato Card, y los dibuja en el template 'home.html'.
 def home(request):
     images = []
-    favourite_list = []
+    favourite_list = services.getAllFavourites(request):#esto llama a la lista de favoritos que esta en el services
 
     return render(request, 'home.html', { 'images': images, 'favourite_list': favourite_list })
 
